6a15d8e18829682afd4a06da7545144348572e10
Teddy Cross <tkazec@gmail.com>
2011-11-24 06:29:53 -0500

    Added support for background images, hrefs, and html in tiles

--- js/background.js
+++ js/background.js
@@ -1,10 +1,18 @@
-var tabbody = document.createElement("body"), tiles = {
+"use strict";
+(function(){
+	
+// x, y, width, height
+// background, image
+// href, app, content
+
+var tiles = {
 	0: {
 		x: 0,
 		y: 0,
 		width: 4,
 		height: 2,
-		background: "lime"
+		background: "lime",
+		image: "icons/128.png"
 	},
 	1: {
 		x: 4,
@@ -25,7 +33,9 @@ var tabbody = document.createElement("body"), tiles = {
 		y: 2,
 		width: 1,
 		height: 1,
-		background: "yellow"
+		background: "yellow",
+		image: "icons/48.png",
+		href: "http://tkaz.ec"
 	},
 	4: {
 		x: 1,
@@ -46,23 +56,39 @@ var tabbody = document.createElement("body"), tiles = {
 		y: 0,
 		width: 4,
 		height: 4,
-		background: "hotpink"
+		background: "hotpink",
+		content: "<p>Hello!</p>"
 	}
 };
 
-Object.keys(tiles).forEach(function(k){
-	var data = tiles[k], el = document.createElement("div");
+var buildtabbody = window.buildtabbody = function(){
+	var tabbody = window.tabbody = document.createElement("body")
 	
-	el.className = "tile";
-	
-	el.style.left = ((100 + 10) * data.x) + "px";
-	el.style.top = ((100 + 10) * data.y) + "px";
-	
-	el.style.width = (100 * data.width) + (10 * (data.width - 1)) + "px";
-	el.style.height = (100 * data.height) + (10 * (data.height - 1)) + "px";
-	
-	el.style.background = data.background;
-	//el.innerHTML = data.content;
-	
-	tabbody.appendChild(el);
-});
+	Object.keys(tiles).forEach(function(k){
+		var data = tiles[k],
+			el = document.createElement(data.href ? "a" : "div"),
+			backgrounds = [];
+		
+		el.style.left = ((100 + 10) * data.x) + "px";
+		el.style.top = ((100 + 10) * data.y) + "px";
+		
+		el.style.width = (100 * data.width) + (10 * (data.width - 1)) + "px";
+		el.style.height = (100 * data.height) + (10 * (data.height - 1)) + "px";
+		
+		data.image && backgrounds.push('url("' + data.image + '") center no-repeat');
+		data.background && backgrounds.push(data.background);
+		el.style.background = backgrounds.join(", ");
+		
+		data.href && el.setAttribute("href", data.href);
+		data.content && (el.innerHTML = data.content);
+		
+		el.className = "tile";
+		tabbody.appendChild(el);
+	});
+};
+
+
+/*** setup ***/
+buildtabbody();
+
+})();
