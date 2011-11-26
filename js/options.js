911c8307d4a69eb56e5515a834a5180543aaebe1
Teddy Cross <tkazec@gmail.com>
2011-11-26 16:43:14 -0500

    Implement the themes page, and organize the js a bit better

--- js/options.js
+++ js/options.js
@@ -1,11 +1,38 @@
-function tabTo(i) {
-	$("body").children(".container").eq(i).add($("#topbar-tabs").children().eq(i)).addClass("active").siblings().removeClass("active");
-}
+/*** tabs ***/
+var tabs = {
+	$logo: $("#topbar-logo"),
+	$buttons: $("#topbar-tabs").children(),
+	$pages: $("body").children(".container"),
+	change: function(i) {
+		tabs.$buttons.eq(i).add(tabs.$pages.eq(i)).addClass("active").siblings().removeClass("active");
+	}
+};
 
-$("#topbar-logo").click(function(){
-	tabTo(4);
+tabs.change(0);
+
+tabs.$logo.click(function(){
+	tabs.change(4);
 });
 
-$("#topbar-tabs").children().click(function(){
-	tabTo($(this).index());
+tabs.$buttons.click(function(){
+	tabs.change($(this).index());
 });
+
+
+/*** theme ***/
+var theme = {
+	$text: $("#theme"),
+	$save: $("#theme-save")
+};
+
+theme.$text.val(localStorage.theme);
+
+theme.$save.click(function(){
+	localStorage.theme = theme.$text.val();
+	
+	theme.$save.prop("disabled", true);
+	
+	theme.$text.one("keydown", function(){
+		theme.$save.prop("disabled", false);
+	});
+}).click();
