cd080ef822940ca85ce34587818ca0259df95519
Teddy Cross <tkazec@gmail.com>
2012-06-01 22:59:23 -0400

    Add some flare to tab.js
    Now does some high precision logging, and polls until the background page is ready.

--- scripts/tab.js
+++ scripts/tab.js
@@ -1,1 +1,23 @@
-document.documentElement.replaceChild(document.importNode(chrome.extension.getBackgroundPage().state.tab, true), document.body);
+(function () { "use strict";
+
+var background;
+
+console.mark = function () {
+	this.log.apply(this, [window.performance.webkitNow().toFixed(10), ":"].concat(Array.prototype.slice.call(arguments)));
+};
+
+console.mark("Spinning up...");
+
+(function init (counter) {
+	console.mark("Init attempt #" + counter + ".");
+	
+	if ((background = chrome.extension.getBackgroundPage()) && background.state && background.state.tab) {
+		document.documentElement.replaceChild(document.importNode(background.state.tab, true), document.body);
+		
+		console.mark("Init complete. Ready to go!");
+	} else {
+		setTimeout(init, counter, ++counter);
+	}
+})(1);
+
+})();
