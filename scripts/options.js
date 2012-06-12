042406170eaaf459a8c0f4371b6925d1b7a12639
Teddy Cross <tkazec@gmail.com>
2012-06-12 01:08:53 -0400

    Redesign the options page
     - Add the Bootstrap JS.
     - Move everything besides the tiles into modals.
     - Redo the theme UI.

--- scripts/options.js
+++ scripts/options.js
@@ -1,44 +1,32 @@
 (function () { "use strict";
 
-/*** tabs ***/
-var tabs = {
-	$logo: $("#nav-logo"),
-	$buttons: $("#nav-tabs").children(),
-	$pages: $("body").children(".container"),
-	change: function (i) {
-		tabs.$buttons.eq(i).add(tabs.$pages.eq(i)).addClass("active").siblings().removeClass("active");
-	}
-};
-
-tabs.$logo.click(function () {
-	tabs.change(3);
-});
-
-tabs.$buttons.click(function () {
-	tabs.change($(this).index());
-});
-
-tabs.change(0);
+/*** setup ***/
+var background = chrome.extension.getBackgroundPage();
 
 
 /*** tiles ***/
-$("#tiles").replaceWith(document.importNode(chrome.extension.getBackgroundPage().state.tab, true));
+$("#tiles").replaceWith(document.importNode(background.state.tab, true));
 
 
 /*** theme ***/
 var theme = {
-	$text: $("#theme"),
+	$type: $("#theme-type > button"),
+	$css: $("#theme-css"),
 	$save: $("#theme-save")
 };
 
-theme.$text.val(Data.get("theme-css"));
+theme.$type.click(function () {
+	Data.set("theme-type", this.value);
+}).filter("[value='" + Data.get("theme-type") + "']").addClass("active");
+
+theme.$css.val(Data.get("theme-css"));
 
 theme.$save.click(function () {
-	Data.set("theme-css", theme.$text.val());
+	Data.set("theme-css", theme.$css.val());
 	
 	theme.$save.prop("disabled", true);
 	
-	theme.$text.one("keydown", function () {
+	theme.$css.one("keydown", function () {
 		theme.$save.prop("disabled", false);
 	});
 }).click();
